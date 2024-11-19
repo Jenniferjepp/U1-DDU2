873520b512522cb76123e23503e8143ca8da3b79
Jenniferjepp <nelliferjj@gmail.com>
2024-11-19 14:55:33 +0100

    City names
    Deklarerar en funktion och for-loop för att skapa alla de 38 knapparna som innehåller stadsnamnen. Funktionen skapar en ny div för varje loop som körs och i denna div så läggs stadens namn till med textcontent.

--- index.js
+++ index.js
@@ -4,3 +4,21 @@
 
 // Recommended: Ask for the city name and then the rest of the code
 
+
+
+const bigCityDiv = document.querySelector("#cities"); // deklarerar konstanten bigCityDiv som refererar till diven som har id:et #cities.
+
+// FUNKTION - skapa city-divar.
+function createCityButton(cityName) {                // deklarerar funktionen "createCityButton" med parametern "cityName" (eftersom att denna kommer att ändras för varje stads namn).
+    const newBox = document.createElement("div");    // skapar en ny div som tilldelas konstanten "newBox".
+    newBox.classList.add("cityBox");                 // lägger till diven (AKA konstanten "newBox") i klassen "cityBox" som redan fanns deklarerad i CSS.
+    newBox.textContent = cityName;                   // lägger till textcontent som finns i "cityName" (parametern) i diven.
+
+    bigCityDiv.append(newBox);                       // appendar den nyskapta diven i den stora diven som redan fanns i HTML-dokumentet som ska innehålla alla city Buttons.
+}
+
+// FOR-LOOP - för att loopa igenom alla city namn.
+for (let i = 0; i<cities.length; i++) {               // deklarerar en for-loop. Här skapas variablen i som startar på siffran (AKA index) 0. Vi skriver  i<cities.length  för att kunna loopa igenom varje objekt i arrayen från index 0 till sista objektet (genom att skriva cities.length kommer vi igenom alla och avslutar på sista objektet). Efter varje loop inkremeras i med 1 och går vidare till nästa index i arrayen.
+createCityButton(cities[i].name)                      // anropar funktionen "createCityButton" med argumentet  (cities[i].name)  AKA indexet av det objektet vi är på. name för att kunna skriva ut värdet som står på name AKA stadens namn.
+}
+
